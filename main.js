db4bcf08a1065c7a1711b967f1f796d58bc467e6
Luis Quintanilla <equalsgains@gmail.com>
2017-04-14 18:17:48 -0600

    Js stalker

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-console.log("Hello");
+var firstName  = prompt("What is your first Name?"),
+    lastName = prompt("and your last name?"),
+    age = prompt("how yougn or old are you?");
+
+alert("Well congradulations " + firstName + " " + lastName + " you know JS and you're only " + age + " years old!");
