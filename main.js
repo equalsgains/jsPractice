505513bb1500bf4f239db67302552c5eb71e7306
Luis Quintanilla <equalsgains@gmail.com>
2017-04-14 18:21:45 -0600

    first commit

--- main.js
+++
@@ -1,5 +0,0 @@
-var firstName  = prompt("What is your first Name?"),
-    lastName = prompt("and your last name?"),
-    age = prompt("how yougn or old are you?");
-
-alert("Well congradulations " + firstName + " " + lastName + " you know JS and you're only " + age + " years old!");
