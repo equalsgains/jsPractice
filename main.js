ea61a0e18146b2dd96baaab7875425ef854b334f
Luis Quintanilla <equalsgains@gmail.com>
2017-04-30 21:16:53 -0600

    added most variables needed

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
-// reverse array
+var limitDisplay = document.getElementById('limit'),
+    p1Display = document.getElementById('p1'),
+    p2Display = document.getElementById('p2'),
+    p1Btn = document.getElementById('c1'),
+    p2Btn = document.getElementById('c2'),
+    resetBtn = document.getElementById('reset'),
+    scoreInput = document.getElementById('scoreLimit'),
+    p1Score = 0,
+    p2Score = 0;
 
-var array = [1, 2, 3, 4, 5];
 
-function reverseArray(arr) {
-    var reversedArr = arr.reverse();
-    reversedArr.forEach(function (list) {
-        console.log(list);
-    })
-};
 
-// is it uniform
+// the input number will be displayed in "limitDisplay"
+// the score will be displayed for every time the input is changed
+scoreInput.addEventListener("change", function(){
+    limitDisplay.textContent = this.value.toString();
+});
 
-var yes = [1, 1, 1, 1, 1, 1, 1],
-    no = [1, 1, 1, 1, 1, "i",];
 
-function isUniform(arr) {
-    var same = arr[0];
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i] !== same) {
-            return false
-        }
-    }
-    return true
 
-};
+
+p1Btn.addEventListener("click", function(){
+    p1Score++;
+    p1Display.textContent = p1Score.toString();
+});
+
+p2Btn.addEventListener("click", function(){
+    p2Score++;
+    p2Display.textContent = p2Score.toString();
+});
+
